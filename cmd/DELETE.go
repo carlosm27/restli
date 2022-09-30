4d107d66b4439d3def9236c3aefd890fbca200df
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-09-30 13:19:12 -0400

    Create DELETE cmd

---
+++ cmd/DELETE.go
@@ -0,0 +1,37 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// DELETECmd represents the DELETE command
+var DELETECmd = &cobra.Command{
+	Use:   "DELETE",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("DELETE called")
+
+		methods.Delete(urlDelete)
+	},
+}
+
+var urlDelete string
+
+func init() {
+	rootCmd.AddCommand(DELETECmd)
+
+	PATCHCmd.Flags().StringVarP(&urlDelete, "url", "u", "", "URL of API you want to request")
+
+}
