265e1561311f4e4c3442f3e9b4b8563e02882ed3
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-09-30 13:37:49 -0400

    Update get.go

--- cmd/get.go
+++ cmd/get.go
@@ -1,20 +1,19 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
 */
 package cmd
 
 import (
 	"fmt"
-
+	
 	"github.com/carlosm27/restli/cmd/methods"
 	"github.com/spf13/cobra"
 )
 
-var url string
-
-// getCmd represents the get command
-var getCmd = &cobra.Command{
-	Use:   "get",
+// GETCmd represents the GET command
+var GETCmd = &cobra.Command{
+	Use:   "GET",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -23,14 +22,14 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("get called")
+		fmt.Println("GET called")
 		methods.Get(url)
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(getCmd)
+	rootCmd.AddCommand(GETCmd)
 
-	getCmd.Flags().StringVarP(&url, "url", "u", "", "URL of API you want to request")
-
+   GETCmd.Flags().StringVarP(&url, "url", "u", "", "URL of API you want to request")
+	
 }
