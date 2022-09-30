4862d2e6fd30b456d76422257fd0a0360fcebee4
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-09-30 12:49:34 -0400

    Add cmd for PATCH HTTP method

---
+++ cmd/methods/patch.go
@@ -0,0 +1,28 @@
+package methods
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/go-resty/resty/v2"
+)
+
+func Put(url string, body string) {
+
+	client := resty.New()
+
+	resp, err := client.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(body).
+		Patch(url)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println(body)
+
+	fmt.Println("Response Body:\n", resp)
+	fmt.Println(" Status Code: ", resp.StatusCode())
+
+}
