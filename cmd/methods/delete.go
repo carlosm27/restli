ebc157e9919ef1f7314aff2c4153ae03f4f78651
Carlos Marcano <c_marcano@correo.unimet.edu.ve>
2022-09-30 21:23:52 -0400

    Delete setbody

--- cmd/methods/delete.go
+++ cmd/methods/delete.go
@@ -13,14 +13,13 @@ func Delete(url string) {
 
 	resp, err := client.R().
 		SetHeader("Content-Type", "application/json").
-		SetBody(body).
 		Delete(url)
 
 	if err != nil {
 		log.Println(err)
 	}
 
-	fmt.Println(body)
+	
 
 	fmt.Println("Response Body:\n", resp)
 	fmt.Println(" Status Code: ", resp.StatusCode())
